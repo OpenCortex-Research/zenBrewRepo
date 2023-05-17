c721db114a25aab68d7f12fd86f5d7f7abac4d38
Judah Fuller <25085396+JudahZF@users.noreply.github.com>
2023-05-17 13:14:21 +0100

    Disable Model Renamer Updates

--- packages/ModelRenamer/update.sh
+++ packages/ModelRenamer/update.sh
@@ -1,2 +1,1 @@
-curl https://github.com/VanIseghemThomas/OpenCortex/raw/main/Model%20Repositories/model_renamer.py -o model_renamer.py
-python model_renamer.py /opt/neuraldsp/OriginalModelRepo.xml /opt/neuraldsp/ModelRepo.xml
+echo To update please uninstall and reinstall the package
